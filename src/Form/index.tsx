7334d85886199e6066b769f8f443e0931a0cd0c2
Mario Cerven <mario.cerven@panaxeo.com>
2023-09-03 00:44:41 +0200

    cleanup form styling

--- src/Form/index.tsx
+++ src/Form/index.tsx
@@ -51,91 +51,84 @@ export default function Form() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-      <div style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "flex-start"
-      }}>
-        <div>
-          <label>
-            First name:
-            <input type="text" {...register("firstName")} />
-            <FieldState control={control} fieldName="firstName" />
-            {/* {errors.firstName?.message} */}
-          </label>
-        </div>
-        <div>
-          <label>
-            Last name:
-            <input type="text" {...register("lastName")} />
-            <FieldState control={control} fieldName="lastName" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Age:
-            <input type="number" {...register("age", { valueAsNumber: true })} min={0} />
-            <FieldState control={control} fieldName="age" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Email:
-            <input type="email" {...register("email")} />
-            <FieldState control={control} fieldName="email" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Gender:{" "}
-            <label>
-              Male
-              <input type="radio" {...register("gender")} value="m" />
-            </label>
-            <label>
-              Female
-              <input type="radio" {...register("gender")} value="f" />
-            </label>
-          </label>
-          <FieldState control={control} fieldName="gender" />
-        </div>
-        <fieldset>
-          <legend>Addresses</legend>
-          {
-            fields.map((field, i) => 
-              <div key={field.id} id={field.id}>
-                <h4>Address {i + 1}</h4>
-                <div>
-                  Country: <input {...register(`addresses.${i}.country`)} />
-                  <p className="error">
-                    {errors.addresses?.[i]?.country?.message}
-                  </p>
-                </div>
-                <div>
-                  City: <input {...register(`addresses.${i}.city`)} />
-                  <p className="error">
-                    {errors.addresses?.[i]?.city?.message}
-                  </p>
-                </div>
-                <button type="button" onClick={() => remove(i)}>Remove</button>
-              </div>)
-          }
-          <button type="button" onClick={() => append({city: "", country: ""})}>Append</button>
-          <FieldState control={control} fieldName="addresses" />
-        </fieldset>
-        
-        <div>          
-          <label>
-            Terms & Conditions:
-            <input type="checkbox" {...register("terms")} />
-          </label>
-          <FieldState control={control} fieldName="terms" />
-          <button type="button" onClick={() => trigger(["terms"])}>Validate Terms & Conditions</button>
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
+      <div>
+        <label>
+          First name:
+          <input type="text" {...register("firstName")} />
+          <FieldState control={control} fieldName="firstName" />
+          {/* {errors.firstName?.message} */}
+        </label>
       </div>
+      <div>
+        <label>
+          Last name:
+          <input type="text" {...register("lastName")} />
+          <FieldState control={control} fieldName="lastName" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Age:
+          <input type="number" {...register("age", { valueAsNumber: true })} min={0} />
+          <FieldState control={control} fieldName="age" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Email:
+          <input type="email" {...register("email")} />
+          <FieldState control={control} fieldName="email" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Gender:{" "}
+          <label>
+            Male
+            <input type="radio" {...register("gender")} value="m" />
+          </label>
+          <label>
+            Female
+            <input type="radio" {...register("gender")} value="f" />
+          </label>
+        </label>
+        <FieldState control={control} fieldName="gender" />
+      </div>
+      <fieldset>
+        <legend>Addresses</legend>
+        {
+          fields.map((field, i) => 
+            <div key={field.id} id={field.id}>
+              <h4>Address {i + 1}</h4>
+              <div>
+                Country: <input {...register(`addresses.${i}.country`)} />
+                <p className="error">
+                  {errors.addresses?.[i]?.country?.message}
+                </p>
+              </div>
+              <div>
+                City: <input {...register(`addresses.${i}.city`)} />
+                <p className="error">
+                  {errors.addresses?.[i]?.city?.message}
+                </p>
+              </div>
+              <button type="button" onClick={() => remove(i)}>Remove</button>
+            </div>)
+        }
+        <button type="button" onClick={() => append({city: "", country: ""})}>Append</button>
+        <FieldState control={control} fieldName="addresses" />
+      </fieldset>
+
+      <div>
+        <label>
+          Terms & Conditions:
+          <input type="checkbox" {...register("terms")} />
+        </label>
+        <FieldState control={control} fieldName="terms" />
+        <button type="button" onClick={() => trigger(["terms"])}>Validate Terms & Conditions</button>
+      </div>
+      <button type="submit">Submit</button>
+
       <div>
         <div>Is dirty: {isDirty.toString()}</div>
         <div>Is valid: {isValid.toString()}</div>
